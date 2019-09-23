98e57ab97a3a4ce87f3174b30fd205941a1c565e
SnowCait <SnowCait@users.noreply.github.com>
2019-09-24 06:57:50 +0900

    Create project

---
+++ MatrixBuildSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixBuildSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
